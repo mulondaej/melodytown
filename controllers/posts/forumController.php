48aa7d295e039945768c995c42b92b1527dae95c
Echizen Ryouma <inktamba@gmail.com>
2024-01-16 16:39:34 +0100

    few changes

--- controllers/posts/forumController.php
+++ controllers/posts/forumController.php
@@ -1,7 +1,9 @@
-<<<<<<< HEAD
 <?php
-// Include any necessary files or configurations
-require_once '../../models/topicsModel.php';
+require_once("../../models/topicsModel.php");
+require_once("../../models/topicAnswersModel.php");
+require_once("../../models/commentsModel.php");
+require_once("../../models/categoriesModel.php");
+require_once("../../models/tagsModel.php");
 
 session_start();
 
@@ -12,6 +14,72 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
+$categories = new Categories;
+$categoriesList = $categories->getList();
+
+$tags = new Tags;
+$tagsList = $tags->getList();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $topic = new Topics;
+    $topic->id_users = 1;
+
+    if (!empty($_POST['tag'])) {
+        $topic->id_tags = $_POST['tag'];
+    } else {
+        if ($_POST['tag'] != $topic->id_tags) {
+            $errors['tag'] = 'Veuillez choisir un tag svp!';
+        } else {
+            $errors['tag'] = 'Le tag ne doit pas etre vide'; 
+        }
+    }
+
+    if (!empty($_POST['categories'])) {
+        $topic->id_categories = $_POST['categories'];
+    } else {
+        if ($_POST['categories'] != $topic->id_tags) {
+            $errors['categories'] = 'Veuillez choisir un tag svp!';
+        } else {
+            $errors['categories'] = 'Le tag ne doit pas etre vide'; 
+        }
+    }
+
+    if (!empty($_POST['title'])) {
+        $topic->title = $_POST['title'];
+        $wordCount = str_word_count($topic->title);
+    } else {
+        if($topic->checkIfExistsByTitle() == 1) {
+            $errors['title'] = TOPICS_TITLE_ERROR_INVALID;
+        } else if ($wordcunt < 8) {
+            $errors['title'] = TOPICS_TITLE_ERROR_INVALID_2;
+        }
+        else {
+            $errors['title'] = TOPICS_TITLE_SUCCESS;
+        }
+    }
+
+    if(!empty($_POST['content'])) {
+        $topic->content = $_POST['content'];
+        $wordCount = str_word_count($topic->content);
+    } else {
+        if ($wordcunt < 30 ) {
+             $errors['content'] = TOPICS_CONTENT_ERROR_INVALID;
+            } else if ($wordcunt < 1) {
+                $errors['content'] = TOPICS_CONTENT_ERROR;
+            }
+         else {
+            $errors['content'] = TOPICS_CONTENT_SUCCESS;
+        }
+    } 
+
+    if (empty($errors)) {
+        if($topic->create()) {
+            $success = 'Votre topic vient d\être publié avec succes';
+        } else {
+            $errors = TOPICS_ERROR;
+        }
+    }
+}
 
 require_once '../../views/parts/header.php';
 
@@ -22,29 +90,4 @@ if ('Location: /forum') {
 }
 
 require_once '../../views/parts/footer.php';
-=======
-<?php
-// Include any necessary files or configurations
-require_once '../../models/topicsModel.php';
-
-session_start();
-
-// Check if the user is logged in. You can implement your authentication logic here.
-if (!isset($_SESSION['user'])) {
-    // Redirect to the login page or display an error message.
-    header("Location: /connexion");
-    exit();
-}
-
-
-require_once '../../views/parts/header.php';
-
-if ('Location: /forum') {
-    require_once '../../views/pages/forum.php'; 
-} else if ('Location: /arena') { 
-    require_once '../../views/pages/arena.php';
-}
-
-require_once '../../views/parts/footer.php';
->>>>>>> 19db88153d4fb2b0737212ded8e024505fda031c
 ?>
