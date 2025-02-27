d9c4874c1321a7cfcb9bfb35541393c7b5066bb6
Jo Tshiba <145117734+mulondaej@users.noreply.github.com>
2025-02-27 05:54:24 +0100

    cool

--- views/parts/header.php
+++ views/parts/header.php
@@ -26,6 +26,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.tiny.cloud/1/r46x7x3vh3lxxmkcnj8zb3ribaidejh4xsiec99z9vj5oudn/tinymce/6/tinymce.min.js"
             referrerpolicy="origin"></script>
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
